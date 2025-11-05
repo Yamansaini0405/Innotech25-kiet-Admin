caccba5de6f77861425a67cc67ed4050695665e3
Yamansaini0405 <sainiyaman14@gmail.com>
2025-11-05 11:48:29 +0530

    fix bug

--- src/pages/ExportDataPage.jsx
+++ src/pages/ExportDataPage.jsx
@@ -22,7 +22,7 @@ export default function ExportDataPage() {
   }, [])
 
   useEffect(() => {
-    if (adminRole !== "departmentAdmin") {
+    if (adminRole !== "superadmin") {
       fetchPanels()
     }
   }, [adminRole])
